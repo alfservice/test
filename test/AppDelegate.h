1c8fc90b2e6ddee5849f396360e255605699639b
邢天阔 <1157291706@qq.com>
2018-01-29 15:08:54 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 邢天阔 on 2018/1/29.
+//  Copyright © 2018年 邢天阔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 邢天阔 on 2018/1/29.
+//  Copyright © 2018年 邢天阔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
